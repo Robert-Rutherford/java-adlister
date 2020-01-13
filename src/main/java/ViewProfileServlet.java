9d1a07b178e2cb181b21abbb49d96c97d9d8b078
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-01-13 13:31:29 -0600

    session exercise

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -8,11 +8,11 @@ import java.io.IOException;
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getSession().getAttribute("user") != null) {
-            response.sendRedirect("/profile");
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
             return;
         }
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        request.getRequestDispatcher("/profile.jsp").forward(request, response);
 //        request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
 }
