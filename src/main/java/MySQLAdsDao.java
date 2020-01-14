038174c3688cb65b7aed6d37a3e41c58edd37d88
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-01-14 14:03:48 -0600

    jdbc exercise

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,6 +1,25 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class MySQLAdsDao implements Ads {
+    Connection connection = null;
+
+
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUsername(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
 
     @Override
     public List<Ad> all() {
