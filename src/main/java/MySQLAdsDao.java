def0b58e7035b990c6bcd7ad805fbc54699c4c82
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-01-14 13:55:26 -0600

    jdbc exercise

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,3 +1,14 @@
-public class MySQLAdsDao {
+import java.util.List;
 
+public class MySQLAdsDao implements Ads {
+
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
 }
