a50d9f258fa68b5a268bd6fb0741b7b9cf803fa7
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-01-15 11:45:40 -0600

    jdbc exercise

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MySQLAdsDao implements Ads {
-    Connection connection = null;
+    private Connection connection;
 
 
     public MySQLAdsDao(Config config) {
