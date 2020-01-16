fd750c2ed0526a98fb55e4a45dec443e35687ac8
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-01-16 10:45:47 -0600

    user exercise

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.dao;
 
 public class DaoFactory {
     private static Ads adsDao;
+    private static Users usersDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
@@ -11,5 +12,10 @@ public class DaoFactory {
         return adsDao;
     }
 
-
+    public static Users getUsersDao() {
+        if (usersDao == null) {
+            usersDao = new MySQLUsersDao(config);
+        }
+        return usersDao;
+    }
 }
